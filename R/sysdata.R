ab67bbf2d30cf07964c74bb39e1f466d0886bf0f
Chebuu <sethcbau@gmail.com>
2020-09-06 00:33:53 -0600

    formatting

--- R/sysdata.R
+++ R/sysdata.R
@@ -1,11 +1,23 @@
+#' @title Dataset `population`
 # population = read.csv('inst/extdata/population.csv')
 # # usethis::use_data(population, overwrite=TRUE)
+
+#' @title Dataset `control`
+#' @details A subset of dataset `population`
 # control = population[is.na(population$interpretation),]
 # # usethis::use_data(cohort, overwrite=TRUE)
+
+#' @title Dataset `cohort`
+#' @details A subset of dataset `population`
 # cohort = population[!is.na(population$interpretation),]
 # # usethis::use_data(control, overwrite=TRUE)
 
+# --- --- ---
+
+#' @title Dataset `orgids`
 # orgids = read.csv('inst/extdata/orgids.csv')
 # # usethis::use_data(orgids, overwrite=TRUE)
-# orgids = read.csv('inst/extdata/staphids.csv')
+
+#' @title Dataset `staphids`
+# staph = read.csv('inst/extdata/staphids.csv')
 # # usethis::use_data(staphids, overwrite=TRUE)
