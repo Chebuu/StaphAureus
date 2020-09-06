a6dff002da4b941720fb8d59b87ec796901c2f7c
Chebuu <sethcbau@gmail.com>
2020-09-06 00:29:48 -0600

    added staphids

--- R/sysdata.R
+++ R/sysdata.R
@@ -1,31 +1,11 @@
-population = read.csv(system.file('extdata/population.csv', package='StaphAureus'))
-usethis::use_data(population, overwrite=TRUE)
-control = population[is.na(population$interpretation),]
-usethis::use_data(cohort, overwrite=TRUE)
-cohort = population[!is.na(population$interpretation),]
-usethis::use_data(control, overwrite=TRUE)
+# population = read.csv('inst/extdata/population.csv')
+# # usethis::use_data(population, overwrite=TRUE)
+# control = population[is.na(population$interpretation),]
+# # usethis::use_data(cohort, overwrite=TRUE)
+# cohort = population[!is.na(population$interpretation),]
+# # usethis::use_data(control, overwrite=TRUE)
 
-orgids = data.frame(
-  org_name = c(
-    "POSITIVE FOR METHICILLIN RESISTANT STAPH AUREUS",
-    "STAPH AUREUS COAG +",
-    "STAPHYLOCOCCUS EPIDERMIDIS",
-    "STAPHYLOCOCCUS HOMINIS",
-    "STAPHYLOCOCCUS LUGDUNENSIS",
-    "STAPHYLOCOCCUS SAPROPHYTICUS, PRESUMPTIVE IDENTIFICATION",
-    "STAPHYLOCOCCUS SPECIES",
-    "STAPHYLOCOCCUS, COAGULASE NEGATIVE",
-    "STAPHYLOCOCCUS, COAGULASE NEGATIVE, PRESUMPTIVELY NOT S. SAPROPHYTICUS",
-  ),
-  org_itemid = c(
-    80293,
-    80023,
-    80024,
-    80162,
-    80255,
-    80138,
-    80155,
-    80262
-  )
-)
-usethis::use_data(orgids, overwrite=TRUE)
+# orgids = read.csv('inst/extdata/orgids.csv')
+# # usethis::use_data(orgids, overwrite=TRUE)
+# orgids = read.csv('inst/extdata/staphids.csv')
+# # usethis::use_data(staphids, overwrite=TRUE)
